d5d5ba63957bfb60b7e4b34dc5cc6c4f6344debe
Xinlue Liu <42654960+XinlueLiu@users.noreply.github.com>
2020-04-19 18:35:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,30 @@ import PIL
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+from skimage import io
+from skimage.transform import rotate,AffineTransform,warp
 
 img = Image.open('./mao.jpg')
 img = np.array(img)
 #plt.imshow(img)
 #plt.show()
 
-#flip
+#greyscale
+gray= cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+plt.imshow(gray)
+#plt.show()
+imggrey = Image.fromarray(gray)
+imggrey.save("grayscale.jpg")
+
+#flip horizontally
 flipped_img = np.fliplr(img)
 plt.imshow(flipped_img)
-img = Image.fromarray(flipped_img)
-img.save("flipped_img.jpg")
+imgflip = Image.fromarray(flipped_img)
+imgflip.save("flipped_img.jpg")
 
-#
+#flip vertically
+vertically_flipped_image = np.flipud(img)
+plt.imshow(vertically_flipped_image)
+imgflip_vert = Image.fromarray(vertically_flipped_image)
+imgflip_vert.save("flipped_img_vert.jpg")
